05f6a505ed99ed296d205f058b893d118be414c5
anshfindoliya <168640161+anshfindoliya@users.noreply.github.com>
2024-05-04 15:43:10 +0530

    Add files via upload

---
+++ operaters.js
@@ -0,0 +1,25 @@
+let x = 'ansh';
+let y = 'findoliya';
+let z
+// x = parseInt(x);
+// x = Number(x);
+
+// x = x.toString();
+
+// x = Boolean(x)
+
+// console.log(Math.sqrt(-1));
+// console.log(1+NaN);
+// console.log(undefined+undefined);
+
+// z = 'ansh'+'findoliya'
+
+// z = 2**3;
+
+// z = 2 == '2';   Equal to (Just the value, not the type)
+// z = 2 === 2;   Equal to (value and type)
+
+// z = 2 !== 2;    Not equal to (Type and value)
+ z = 2 != '2';  // Not equal to (Just the value, not the type)
+
+console.log(z,typeof z);
